c7a7f35b0189873c4621be8ea244075907804e50
Gleb Grenkin <glebgrenkin@gmail.com>
2017-12-19 20:48:09 +1000

    Add to integrands

--- joker-fem-2d/integrands2d.h
+++ joker-fem-2d/integrands2d.h
@@ -1,6 +1,7 @@
 #ifndef INTEGRANDS2D_H_INCLUDED
 #define INTEGRANDS2D_H_INCLUDED
 
+#include <algorithm>
 #include "splines2d.h"
 
 class Integrand {
@@ -35,6 +36,30 @@ public:
     double Integrate (int boundary_edge_index);
 };
 
+double Integrate (Integrand& integrand)
+{
+    double ans = 0.0;
+    for (std::list<int>::iterator i = integrand.support.begin();
+        i != integrand.support.end(); ++i)
+    {
+        int triangle_index = *i;
+        ans += integrand.Integrate(triangle_index);
+    }
+    return ans;
+}
+
+double BoundaryIntegrate (BoundaryIntegrand& integrand)
+{
+    double ans = 0.0;
+    for (std::list<int>::iterator i = integrand.boundary_support.begin();
+        i != integrand.boundary_support.end(); ++i)
+    {
+        int boundary_edge_index = *i;
+        ans += integrand.Integrate(boundary_edge_index);
+    }
+    return ans;
+}
+
 struct BasisFunction {
     Mesh& mesh;
 public:
@@ -66,20 +91,24 @@ public:
     // function value at the point with local coordinates L0, L1
     double Value (int triangle_index, double L0, double L1)
     {
-        int* nodes = mesh.triangles[triangle_index].nodes;
-        return L0 * values[nodes[0]] + L1 * values[nodes[1]]
-            + (1 - L0 - L1) * values[nodes[2]];
+        return get_value(mesh, values, triangle_index, L0, L1);
     }
 
     // function value at the point with parameter t (from -1 to 1)
     double BoundaryValue (int boundary_edge_index, double t)
     {
-        int* nodes = mesh.boundary_edges[boundary_edge_index].boundary_nodes;
-        return 0.5 * (1 - t) * boundary_values[nodes[0]]
-            + 0.5 * (1 + t) * boundary_values[nodes[1]];
+        return get_boundary_value(mesh, values, boundary_edge_index, t);
     }
-
 };
+
+std::list<int> intersect_supports (std::list<int>& s1, std::list<int>& s2)
+{
+    std::list<int> ans;
+    for (std::list<int>::iterator i = s1.begin(); i != s1.end(); ++i) {
+        if (std::find(s2.begin(), s2.end(), *i) != s2.end())
+            ans.push_back(*i);
+    }
+}
 
 struct Mult_P1_Basis : public Integrand {
     FunctionP1& p1;
@@ -87,7 +116,9 @@ struct Mult_P1_Basis : public Integrand {
 
     Mult_P1_Basis (Mesh& _mesh, FunctionP1& _p1, BasisFunction& _basis)
         : Integrand(_mesh), p1(_p1), basis(_basis)
-    {}
+    {
+        support = basis.support;
+    }
 
     double Value (int triangle_index, double L0, double L1)
     {
@@ -119,7 +150,10 @@ struct BoundaryMult_P0_Basis_Basis : public BoundaryIntegrand {
     BoundaryMult_P0_Basis_Basis (Mesh& _mesh, BoundaryFunctionP0& _p0,
         BasisFunction& _basis1, BasisFunction& _basis2)
         : BoundaryIntegrand(_mesh), p0(_p0), basis1(_basis1), basis2(_basis2)
-    {}
+    {
+        boundary_support = intersect_supports(basis1.boundary_support,
+            basis2.boundary_support);
+    }
 
     double Value (int boundary_edge_index, double t)
     {
